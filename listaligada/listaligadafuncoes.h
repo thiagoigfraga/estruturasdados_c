8dfe77fe7bdf46515320a67b7e5a8f30997bf5b9
Thiago Garcia <thiagogarciafraga99@gmail.com>
2022-08-11 00:30:36 -0300

    Atualização código lista ordenada estática, função de inserção

--- listaligada/listaligadafuncoes.h
+++ listaligada/listaligadafuncoes.h
@@ -72,29 +72,55 @@ int obterNo(LISTA *l){
 
 bool inserirElemListaOrd(LISTA *l, REGISTRO reg){
 
+    //Se a lista estiver cheia, o dispo estará como -1 porque não haverá mais elementos disponíveis para inserir,
+    // retorno false (0) imediato
     if(l->dispo == INVALIDO) return false;
 
+    //Como ele começa a varrer a lista pelo início, o anterior ao início é -1 (não existe)
     int ant = INVALIDO;
-    int i = l->inicio;
-    TIPOCHAVE ch = reg.chave;
+    int i = l->inicio; //(Se for a primeira inserção, o l->inicio será -1)
+    TIPOCHAVE ch = reg.chave; //A chave do registro a ser inserido
 
+    /* Se o i não for inválido (precisa varrer a lista até o fim) e a chave a ser inserida é maior que o
+       index buscado, então ele vai varrer a lista até que encontre a posição em que a chave a ser inserida seja menor
+       (Esse while é ignorado se for a primeira inserção)
+    */
     while((i != INVALIDO) && (l->A[i].reg.chave < ch)){
         ant = i;
         i= l->A[i].prox;
     }
+
+    /*Após encontrar o index ele valida se a chave que está sendo procurada já existe na lista, se já existir ele
+     retorna false (regra da implementação)
+     (Esse while é ignorado se for a primeira inserção ou se o index não for encontrado na lista)
+     */
     if(i != INVALIDO && l->A[i].reg.chave == ch) return false;
 
+    //Ele obtém o nó do elemento disponível mais próximo e insere em i
     i = obterNo(l);
+
+    //Insere o registro da inserção no registro do elemento que está disponível
     l->A[i].reg = reg;
 
+    //Se o anterior é inválido, significa que a inserção está sendo feita na primeira posição da lista
     if(ant == INVALIDO){
+        // O -1 que estava em início agora é o prox da primeira posição, assumindo que só existe um elemento e a
+        //   lista para por aí.
         l->A[i].prox = l->inicio;
+
+        //O início agora é 0
         l->inicio = i;
+
+    //Do contrário, se assume que o elemento a ser inserido tem um elemento anterior a ele
     }else{
+        //Agora o próximo do elemento atual recebe o próximo do seu elemento anterior, pela ordenação.
         l->A[i].prox = l->A[ant].prox;
+
+        //O próximo do anterior agora aponta pro elemento que agora foi inserido.
         l->A[ant].prox = i;
     }
 
+    //Processo conclúido, mensagem true (1)
     return true;
 }
 
@@ -103,21 +129,21 @@ void devolverNo(LISTA *l, int j){
     l->dispo = j;
 }
 
-bool excluirElemLista(LISTA *l, TIPOCHAVE ch){
+bool excluirElemLista(LISTA* l, TIPOCHAVE ch) {
+
     int ant = INVALIDO;
     int i = l->inicio;
-
-    while((i != INVALIDO) && (l->A[i].reg.chave < ch)){
+    while ((i != INVALIDO) && (l->A[i].reg.chave<ch)) {
         ant = i;
         i = l->A[i].prox;
     }
-
-    if(i != INVALIDO || l->A[i].reg.chave != ch) return false;
-    if( i == INVALIDO) l->inicio = l->A[i].prox;
+    if (i==INVALIDO || l->A[i].reg.chave!=ch) return false;
+    if (ant == INVALIDO) l->inicio = l->A[i].prox;
     else l->A[ant].prox = l->A[i].prox;
     devolverNo(l,i);
     return true;
 }
+
 
 void reinicializarLista(LISTA *l){
     inicializarLista(l);
--- main.c
+++ main.c
@@ -6,16 +6,24 @@ int main() {
 
     LISTA l;
     REGISTRO reg1,reg2,reg3,reg4;
-    reg1.chave = 1;
-    reg2.chave = 2;
-    reg3.chave = 4;
-    reg4.chave = 3;
+    reg1.chave = 4;
+    reg2.chave = 7;
+    reg3.chave = 5;
+    reg4.chave = 8;
+
+
 
     inicializarLista(&l);
     inserirElemListaOrd(&l, reg1);
+    inserirElemListaOrd(&l, reg2);
+    inserirElemListaOrd(&l, reg3);
+    inserirElemListaOrd(&l, reg4);
+
+
+    excluirElemLista(&l,4);
+    excluirElemLista(&l,5);
+
+
+
     exibirLista(&l);
-
-
-
-
 }
